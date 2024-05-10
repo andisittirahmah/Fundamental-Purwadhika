61804c8594e716bc40d1d774e2e6913576d4cd70
andisittirahmah <asrahmah00@gmail.com>
2024-05-10 22:01:15 +0700

    update exercise day05

--- day05/exercise.js
+++ day05/exercise.js
@@ -1,5 +1,7 @@
-// SLIDE-01
-// 01
+// // SLIDE-01
+// // 01 Write a function to get the lowest, highest and average value in the array (with and without sort function). 
+// Example : arr = [12, 5, 23, 18, 4, 45, 32] → {lowest : 4, highest: 45, average: 19.8xxx}
+
 function FindLowestHighestAndAverage(arrNumbers){
     let lowest = arrNumbers[0]
     let highest = arrNumbers[0]
@@ -19,14 +21,19 @@ console.log(FindLowestHighestAndAverage([12, 5, 23, 18, 4, 45, 32]))
 
 
 
-// 02
+// // 02 Write a function that takes an array of words and returns a string by concatenating the words in the array,
+// separated by commas and - the last word - by an 'and'.
+// a. Example : arr = ["apple", "banana", "cherry", "date"] → “apple,banana,cherry, and date”
+
 function ConcateningByCommaAndByAnd(arrStr){
     return arrStr.slice(0, arrStr.length-1).join(', ') + ' and ' + arrStr[arrStr.length-1]
 }
 
 console.log(ConcateningByCommaAndByAnd(['Banana', 'Cherry', 'Melon', 'Apple']))
 
-// 03
+// 03 Write a function to calculate each element in the same position from two arrays of integer. Assume both arrays are of the same length.
+// example : [1, 2, 3] + [3, 2, 1] → [4, 4, 4]
+
 function SumElementOfTwoArrays(arr1, arr2){
     const result = []
     arr1.forEach((number, index) => {
@@ -37,7 +44,11 @@ function SumElementOfTwoArrays(arr1, arr2){
 }
 console.log(SumElementOfTwoArrays([1, 2, 3], [3, 2, 1]))
 
-// 04
+
+// // 04 Write a function that adds an element to the end of an array. However, the element should only be added if it is not already in the array.
+// Example : arr = [1, 2, 3, 4], newElement = 4 → [1, 2, 3, 4]
+// Example : arr = [1, 2, 3, 4], newElement = 7 → [1, 2, 3, 4, 7]
+
 function FilteringNewElement(arr, newElement){
     if(!arr.includes(newElement)) arr.push(newElement)
 
@@ -46,22 +57,30 @@ function FilteringNewElement(arr, newElement){
 
 console.log(FilteringNewElement([1, 5, 10, 15], 3))
 
-// SLIDE-02
-// 01
+// // SLIDE-02
+// // 01 Write a function to insert multiple given integer (not an array) to an array and have a maximum size input. The
+// array can only have a maximum size from a given input. (if the maximum size of the given input is 5 than the array can only contain 5 elements).
+// Example : maxSize = 5, given integers is 5, 10, 24, 3, 6, 7, 8
+// The function will return [5, 10, 24, 3, 6]
+
 function SliceArrayBasedOnMaxSize(maxSize, ...numbers){
     return numbers.slice(0, maxSize)
 }
 
 console.log(SliceArrayBasedOnMaxSize(5, 10, 3, 7, 100, 25, 55))
 
-// 02
+// // 02 Write a function that will combine 2 given array into one array
+// Example : arr1 = [1, 2, 3], arr2 = [4, 5, 6] → [1, 2, 3, 4, 5, 6]
+
 function CombineTwoArray(arr1, arr2){
     return [...arr1, ...arr2]
 }
 
 console.log(CombineTwoArray([1, 2, 3], [4, 5, 6]))
 
-// 03
+// // 03 Write a function to find duplicate values in an array
+// Example : arr = [1, 2, 2, 2, 3, 3, 4, 5, 5] → [2, 3, 5]
+
 function FindDuplicate(arrNumbers){
     const arrResult = []
     arrNumbers.forEach(number => {
@@ -79,7 +98,9 @@ console.log(FindDuplicate([1, 2, 2, 3, 3]))
 
 
 
-// 04
+// // 04 Write a function to find the difference in 2 given array
+// Example : arr1 = [1, 2, 3, 4, 5], arr2 = [3, 4, 5, 6, 7] → [1, 2, 6, 7]
+
 function FindDifferenceElementInTwoArray(arr1, arr2){
     const differentElement = []
 
@@ -103,3 +124,40 @@ function FindDifferenceElementInTwoArray(arr1, arr2){
 }
 
 console.log(FindDifferenceElementInTwoArray([1, 3, 5], [2, 4, 5]))
+
+// SLIDE-03
+// 01 Based on the array below write a function that will return primitive data types only. let arr = [1, [], undefined, {}, "string", {}, []];
+// The function will return [1, undefined, “string”]
+
+function ShowPrimitiveData(arrRandom){
+    const arrPrimitiveData = []
+    arrRandom.forEach(data => {
+       if(typeof data !== 'object') arrPrimitiveData.push(data)
+    })
+    return arrPrimitiveData
+}
+console.log(ShowPrimitiveData([1, [], undefined, {}, 'string', {}, []]))
+
+// 02 Write a function from a given array of numbers and return the second smallest number
+// Example : numbers = [5, 3, 1, 7, 2, 6] → 2
+
+function FindSecondSmallest(arrNumbers){    
+    const sortedArrNumbers = arrNumbers.sort((low, high) => low - high)
+    return sortedArrNumbers[1]
+}
+
+console.log(FindSecondSmallest([10, 5, 100, 25, 3, 1, 5, 1000]))
+
+// 03 Write a function from a given array of mixed data types and return the sum of all the number
+// Example : mixedArray = ["3", 1, "string", null, false, undefined, 2] → 3
+
+function SumTypeOfNumber(arrData){
+    let result = 0
+
+    arrData.forEach(data => {
+        if(typeof data === 'number') result += data 
+    })
+
+    return result 
+}
+
